f22001e9756627f28ae43d089d289c4bfc967a68
MuGuiLin <muguilin@foxmail.com>
2020-11-30 23:32:38 +0800

    update

--- Koa/Koa-MongoDB/router/api-router.js
+++ Koa/Koa-MongoDB/router/api-router.js
@@ -156,6 +156,15 @@ module.exports = KoaRouter
         await ctx.render('about', data);
     })
 
+     // 关于页
+     .get('/news', async (ctx, next) => {
+        const data = {
+            name: '沐枫',
+            list: ['HTML5', 'CSS3', 'ES6', 'Node.js', 'Vue', 'React', 'Angular']
+        }
+        await ctx.render('news', data);
+    })
+
     // 用户列表
     .get('/users', async (ctx, next) => {
         const data = {
